f77f9836d79f09d5d994be7af997956be1cc511f
Oyoshi <rafal.apriasz@gmail.com>
2022-09-20 22:49:58 +0200

    feat: use google api client to authand fetch playlists

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
 #!/usr/bin/python3
-from pytube import Playlist
-import os
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+
+class YouTubePlaylistsDownloader:
+    def __init__(self):
+        credentials = self.get_credentials()
+        self.youtube = build("youtube", "v3", credentials=credentials)
+
+    def get_credentials(self):
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "client_secrets.json",
+            scopes=["https://www.googleapis.com/auth/youtube.readonly"],
+        )
+        flow.run_local_server(port=8080, prompt="consent")
+        return flow.credentials
+
+    def execute(self):
+        result = {}
+        mine_playlists = self.get_mine_playlists()
+        for playlist in mine_playlists:
+            playlist_name = playlist["snippet"]["title"]
+            playlist_id = playlist["id"]
+            videos = [
+                video["contentDetails"]["videoId"]
+                for video in self.get_all_videos(playlist_id)
+            ]
+            result[playlist_name] = videos
+        return result
+        # yt_url = f"https://youtu.be/{video_id}"
+
+    def get_mine_playlists(self):
+        request = self.youtube.playlists().list(part="snippet", mine=True)
+        response = request.execute()
+        return response["items"]
+
+    def get_all_videos(self, playlist_id):
+        request = self.youtube.playlistItems().list(
+            part="contentDetails", playlistId=playlist_id
+        )
+        response = request.execute()
+        return response["items"]
 
 
 def main():
-    p = Playlist(
-        "https://www.youtube.com/playlist?list=PLqgbMHsZiLUDrqIHDH8SDc1D06l5Mwefb"
-    )
-    print(f"Downloading: {p.title}")
-    for video in p.videos:
-        out_file = (
-            video.streams.filter(only_audio=True).first().download(output_path=".")
-        )
-        base, ext = os.path.splitext(out_file)
-        new_file = base + ".mp3"
-        os.rename(out_file, new_file)
+    downloader = YouTubePlaylistsDownloader()
+    print(downloader.execute())
 
 
 if __name__ == "__main__":
